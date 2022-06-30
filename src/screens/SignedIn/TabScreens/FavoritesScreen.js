10164eba9d5b46e393bd3340292048f8d19df0da
rzvnbaba <razvanbnsr@gmail.com>
2022-06-30 19:46:01 +0200

    Updated favorites screen

--- src/screens/SignedIn/TabScreens/FavoritesScreen.js
+++ src/screens/SignedIn/TabScreens/FavoritesScreen.js
@@ -1,10 +1,33 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {SafeAreaView, Text, TouchableOpacity, View} from 'react-native';
 
 const FavoriteScreen = () => {
   return (
     <SafeAreaView>
-      <Text>Favorite</Text>
+      <View style={{marginTop: 130}}>
+        <Text style={{textAlign: 'center', fontSize: 18, fontWeight: '600'}}>
+          Non hai alcuno store preferito
+        </Text>
+        <Text
+          style={{textAlign: 'center', marginHorizontal: 30, marginTop: 20}}>
+          Tocca il cuore accanto ad uno store per aggiungerlo alla tua lista
+          degli store preferiti
+        </Text>
+      </View>
+      <View style={{alignItems: 'center'}}>
+        <TouchableOpacity
+          style={{
+            backgroundColor: '#1dc279',
+            borderRadius: 20,
+            shadowOpacity: 0.5,
+            shadowColor: '#969696',
+            marginTop: 50,
+          }}>
+          <Text style={{fontSize: 18, fontWeight: '600', padding: 10}}>
+            Esplora gli store
+          </Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 };

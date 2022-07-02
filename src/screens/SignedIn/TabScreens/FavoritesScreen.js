7dc14b51b8f4407fcde8bf631c06fb450aa0e5d6
rzvnbaba <razvanbnsr@gmail.com>
2022-07-02 19:26:13 +0200

    Updated favorite screen style

--- src/screens/SignedIn/TabScreens/FavoritesScreen.js
+++ src/screens/SignedIn/TabScreens/FavoritesScreen.js
@@ -5,11 +5,22 @@ const FavoriteScreen = ({navigation}) => {
   return (
     <SafeAreaView>
       <View style={{marginTop: 130}}>
-        <Text style={{textAlign: 'center', fontSize: 18, fontWeight: '600'}}>
+        <Text
+          style={{
+            textAlign: 'center',
+            fontSize: 18,
+            fontWeight: '600',
+            color: '#000000',
+          }}>
           Non hai alcuno store preferito
         </Text>
         <Text
-          style={{textAlign: 'center', marginHorizontal: 30, marginTop: 20}}>
+          style={{
+            textAlign: 'center',
+            marginHorizontal: 30,
+            marginTop: 20,
+            color: '#000000',
+          }}>
           Tocca il cuore accanto ad uno store per aggiungerlo alla tua lista
           degli store preferiti
         </Text>
@@ -24,7 +35,13 @@ const FavoriteScreen = ({navigation}) => {
             marginTop: 50,
           }}
           onPress={() => navigation.navigate('Home')}>
-          <Text style={{fontSize: 18, fontWeight: '600', padding: 10}}>
+          <Text
+            style={{
+              fontSize: 18,
+              fontWeight: '600',
+              padding: 10,
+              color: '#ffffff',
+            }}>
             Esplora gli store
           </Text>
         </TouchableOpacity>

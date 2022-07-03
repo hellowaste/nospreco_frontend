03da4b34c8d0ac717b3cd62fab6f4d68b86d59b7
rzvnbaba <razvanbnsr@gmail.com>
2022-07-03 12:31:02 +0200

    Updated orders history screen

--- src/screens/SignedIn/PagesScreens/Settings/Orders/OrdersHistory/OrdersHistoryScreen.js
+++ src/screens/SignedIn/PagesScreens/Settings/Orders/OrdersHistory/OrdersHistoryScreen.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {SafeAreaView, Text, TouchableOpacity, View} from 'react-native';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const OrdersHistoryScreen = ({navigation}) => {
+  const [inProgress, setInProgress] = useState(true);
   return (
     <SafeAreaView style={{backgroundColor: '#eee', flex: 1}}>
       <View style={{flexDirection: 'row', alignItems: 'center'}}>
@@ -16,6 +17,107 @@ const OrdersHistoryScreen = ({navigation}) => {
         <Text style={{color: '#000000', fontSize: 18, fontWeight: '700'}}>
           Storico ordini
         </Text>
+      </View>
+      <View style={{marginHorizontal: 10, marginTop: 10}}>
+        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+          {inProgress ? (
+            <View
+              style={{
+                backgroundColor: '#ffffff',
+                borderRadius: 15,
+                width: '50%',
+              }}>
+              <Text
+                style={{
+                  color: '#00807a',
+                  fontSize: 16,
+                  fontWeight: '600',
+                  paddingVertical: 10,
+                  textAlign: 'center',
+                }}>
+                In corso
+              </Text>
+            </View>
+          ) : (
+            <TouchableOpacity
+              style={{
+                borderRadius: 15,
+                width: '50%',
+              }}
+              onPress={() => setInProgress(true)}>
+              <Text
+                style={{
+                  color: '#000000',
+                  fontSize: 16,
+                  fontWeight: '600',
+                  paddingVertical: 10,
+                  textAlign: 'center',
+                }}>
+                In corso
+              </Text>
+            </TouchableOpacity>
+          )}
+          {inProgress ? (
+            <TouchableOpacity
+              style={{
+                borderRadius: 15,
+                width: '50%',
+              }}
+              onPress={() => setInProgress(false)}>
+              <Text
+                style={{
+                  color: '#000000',
+                  fontSize: 16,
+                  fontWeight: '600',
+                  paddingVertical: 10,
+                  textAlign: 'center',
+                }}>
+                Conclusi
+              </Text>
+            </TouchableOpacity>
+          ) : (
+            <View
+              style={{
+                backgroundColor: '#ffffff',
+                borderRadius: 15,
+                width: '50%',
+              }}>
+              <Text
+                style={{
+                  color: '#00807a',
+                  fontSize: 16,
+                  fontWeight: '600',
+                  paddingVertical: 10,
+                  textAlign: 'center',
+                }}>
+                Conclusi
+              </Text>
+            </View>
+          )}
+        </View>
+      </View>
+      <View style={{marginTop: 50}}>
+        {inProgress ? (
+          <Text
+            style={{
+              textAlign: 'center',
+              color: '#000000',
+              fontSize: 16,
+              fontWeight: '600',
+            }}>
+            Non hai ordini in corso
+          </Text>
+        ) : (
+          <Text
+            style={{
+              textAlign: 'center',
+              color: '#000000',
+              fontSize: 16,
+              fontWeight: '600',
+            }}>
+            Non hai ordini conclusi
+          </Text>
+        )}
       </View>
     </SafeAreaView>
   );
